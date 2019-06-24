c54afccbcd86b9a101f165c6a342d9e609670542
phucqd <quach.dai.phuc@framgia.com>
2019-06-24 09:22:19 +0700

    init project test package

---
+++ src/SayHello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: quach.dai.phuc
+ * Date: 24/06/2019
+ * Time: 09:13
+ */
+
+namespace HelloWorld;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
